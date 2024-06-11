630e51e826a256449e9d63524ea882a0f4a3d17b
jfdezd <javierfernandezdominguez@gmail.com>
2024-06-11 22:35:46 +0200

    first micro

--- back/db.js
+++ back/db.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 // db.js
 const mongoose = require('mongoose');
 
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
 function connectDB() {
-  return mongoose.connect('mongodb+srv://admin:Zawl4rwiBmzQ8cv7@aqnit.o1mc6xz.mongodb.net/aquanima?retryWrites=true&w=majority&appName=aqnit', {useNewUrlParser: true, useUnifiedTopology: true});
+  console.log(dbUser);
+  console.log(dbPassword);
+  return mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@aqnit.o1mc6xz.mongodb.net/aquanima?retryWrites=true&w=majority&appName=aqnit`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 }
 
 
